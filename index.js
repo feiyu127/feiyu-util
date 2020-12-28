aa24cfbe26af5b0aa51b648a08b3a5a9c93ff9a8
nanshouxiao <nanshouxiao@teacher.com.cn>
2020-12-28 11:05:40 +0800

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = require('./lib/feiyuUtil');
+const fileUtil = require('./core/feiyuUtil');
+const PromisePool = require('./core/PromisePool');
+
+module.exports = {
+	fileUtil,
+	PromisePool,
+}
